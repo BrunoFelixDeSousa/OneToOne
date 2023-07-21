10dcdbf7a2087572132d2fc061e1f8a55c860de4
Bruno Felix <brunofelixdesousa@hotmail.com>
2023-07-21 02:17:42 -0300

    ✅ Post CriarPessoa

--- src/main/java/br/com/bfelix/OneToOne/controllers/PessoaController.java
+++ src/main/java/br/com/bfelix/OneToOne/controllers/PessoaController.java
@@ -1,8 +1,10 @@
 package br.com.bfelix.OneToOne.controllers;
 
+import br.com.bfelix.OneToOne.DTO.PessoaEnderecoDTO;
+import br.com.bfelix.OneToOne.entities.Endereco;
+import br.com.bfelix.OneToOne.entities.Pessoa;
+import br.com.bfelix.OneToOne.services.PessoaService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,17 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.bfelix.OneToOne.DTO.PessoaEnderecoDTO;
-import br.com.bfelix.OneToOne.entities.Endereco;
-import br.com.bfelix.OneToOne.entities.Pessoa;
-import br.com.bfelix.OneToOne.services.PessoaService;
-
 @Slf4j
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
-
-	private static final Logger logger = LoggerFactory.getLogger(PessoaController.class);
 
 	private final PessoaService pessoaService;
 
@@ -30,8 +25,6 @@ public class PessoaController {
 	
 	@PostMapping
 	public ResponseEntity<Object> CriarPessoa(@RequestBody PessoaEnderecoDTO pessoaEnderecoDTO) {
-
-		System.out.println("======>" + pessoaEnderecoDTO.getEnderecoDTO().getRua());
 
 		Pessoa pessoa = new Pessoa();
 		pessoa.setNome(pessoaEnderecoDTO.getNome());
@@ -43,7 +36,7 @@ public class PessoaController {
         
         pessoaService.salvarPessoaComEndereco(pessoa, endereco);
         
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body("Pessoa cadastrado com sucesso!");
 	}
-	
+
 }
